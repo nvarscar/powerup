3a59c66903ec5bf99fd389bd1d2c332a77f77ce4
Kirill Kravtsov <kkravtsov@wawanesa.com>
2018-02-12 16:50:50 -0600

    Adding configuration file tests

--- Tests/Get-PowerUpConfig.Tests.ps1
+++ Tests/Get-PowerUpConfig.Tests.ps1
@@ -6,7 +6,6 @@ Describe "$commandName tests" {
 	It "Should return empty configuration when path does not exist" {
 		$result = Get-PowerUpConfig 'asdqweqsdfwer'
 		$result.ApplicationName | Should Be $null
-		$result.Build | Should Not Be $null
 		$result.SqlInstance | Should Be $null
 		$result.Database | Should Be $null
 		$result.DeploymentMethod | Should Be $null
@@ -15,8 +14,40 @@ Describe "$commandName tests" {
 		$result.Credential | Should Be $null
 		$result.Username | Should Be $null
 		$result.Password | Should Be $null
-		$result.LogToTable | Should Be $null
+		$result.SchemaVersionTable | Should Be $null
 		$result.Silent | Should Be $null
+		$result.Variables | Should Be $null
+	}
+
+	It "Should return empty configuration from empty config file" {
+		$result = Get-PowerUpConfig "$here\etc\empty_config.json"
+		$result.ApplicationName | Should Be $null
+		$result.SqlInstance | Should Be $null
+		$result.Database | Should Be $null
+		$result.DeploymentMethod | Should Be $null
+		$result.ConnectionTimeout | Should Be $null
+		$result.Encrypt | Should Be $null
+		$result.Credential | Should Be $null
+		$result.Username | Should Be $null
+		$result.Password | Should Be $null
+		$result.SchemaVersionTable | Should Be $null
+		$result.Silent | Should Be $null
+		$result.Variables | Should Be $null
+	}
+
+	It "Should return all configurations from the config file" {
+		$result = Get-PowerUpConfig "$here\etc\full_config.json"
+		$result.ApplicationName | Should Be "MyTestApp"
+		$result.SqlInstance | Should Be "TestServer"
+		$result.Database | Should Be "MyTestDB"
+		$result.DeploymentMethod | Should Be "SingleTransaction"
+		$result.ConnectionTimeout | Should Be 40
+		$result.Encrypt | Should Be $null
+		$result.Credential | Should Be $null
+		$result.Username | Should Be "TestUser"
+		$result.Password | Should Be "TestPassword"
+		$result.SchemaVersionTable | Should Be "test.Table"
+		$result.Silent | Should Be $true
 		$result.Variables | Should Be $null
 	}
 
