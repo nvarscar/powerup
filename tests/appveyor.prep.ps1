f74858f585ea593d60075dfd42ddf633ccf6e0de
Kirill Kravtsov <kkravtsov@wawanesa.com>
2018-02-20 15:49:25 -0600

    Format me nicely?

--- tests/appveyor.prep.ps1
+++ tests/appveyor.prep.ps1
@@ -1,5 +1,5 @@
-﻿Write-Host -Object "appveyor.prep: Cloning lab materials"  -ForegroundColor DarkGreen
-git clone -q --branch=master --depth=1 https://github.com/sqlcollaborative/appveyor-lab.git C:\github\appveyor-lab
+﻿# Write-Host -Object "appveyor.prep: Cloning lab materials"  -ForegroundColor DarkGreen
+# git clone -q --branch=master --depth=1 https://github.com/sqlcollaborative/appveyor-lab.git C:\github\appveyor-lab
 #Install codecov to upload results
 # Write-Host -Object "appveyor.prep: Install codecov" -ForegroundColor DarkGreen
 # choco install codecov | Out-Null
@@ -11,3 +11,4 @@ git clone -q --branch=master --depth=1 https://github.com/sqlcollaborative/appve
 # "Get Pester manually"
 Write-Host -Object "appveyor.prep: Install Pester" -ForegroundColor DarkGreen
 Install-Module -Name Pester -Repository PSGallery -Force | Out-Null
+Install-Module -Name Assert -Repository PSGallery -Force | Out-Null
