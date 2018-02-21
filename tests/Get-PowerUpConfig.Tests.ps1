8ba76e48d7b8dc019ae477b918e76ee4d894eb87
Kirill Kravtsov <kkravtsov@wawanesa.com>
2018-02-21 13:04:30 -0600

    Tests for overrides

--- tests/Get-PowerUpConfig.Tests.ps1
+++ tests/Get-PowerUpConfig.Tests.ps1
@@ -7,6 +7,38 @@ Describe "$commandName tests" {
 		{ $result = Get-PowerUpConfig 'asdqweqsdfwer' } | Should throw
 	}
 
+	It "Should return an empty config by default" {
+		$result = Get-PowerUpConfig
+		$result.ApplicationName | Should Be $null
+		$result.SqlInstance | Should Be $null
+		$result.Database | Should Be $null
+		$result.DeploymentMethod | Should Be $null
+		$result.ConnectionTimeout | Should Be $null
+		$result.Encrypt | Should Be $null
+		$result.Credential | Should Be $null
+		$result.Username | Should Be $null
+		$result.Password | Should Be $null
+		$result.SchemaVersionTable | Should Be $null
+		$result.Silent | Should Be $null
+		$result.Variables | Should Be $null
+	}
+
+	It "Should override properties in an empty config" {
+		$result = Get-PowerUpConfig -Configuration @{ApplicationName = 'MyNewApp'; ConnectionTimeout = 3}
+		$result.ApplicationName | Should Be 'MyNewApp'
+		$result.SqlInstance | Should Be $null
+		$result.Database | Should Be $null
+		$result.DeploymentMethod | Should Be $null
+		$result.ConnectionTimeout | Should Be 3
+		$result.Encrypt | Should Be $null
+		$result.Credential | Should Be $null
+		$result.Username | Should Be $null
+		$result.Password | Should Be $null
+		$result.SchemaVersionTable | Should Be $null
+		$result.Silent | Should Be $null
+		$result.Variables | Should Be $null
+	}
+
 	It "Should return empty configuration from empty config file" {
 		$result = Get-PowerUpConfig "$here\etc\empty_config.json"
 		$result.ApplicationName | Should Be $null
@@ -30,6 +62,22 @@ Describe "$commandName tests" {
 		$result.Database | Should Be "MyTestDB"
 		$result.DeploymentMethod | Should Be "SingleTransaction"
 		$result.ConnectionTimeout | Should Be 40
+		$result.Encrypt | Should Be $null
+		$result.Credential | Should Be $null
+		$result.Username | Should Be "TestUser"
+		$result.Password | Should Be "TestPassword"
+		$result.SchemaVersionTable | Should Be "test.Table"
+		$result.Silent | Should Be $true
+		$result.Variables | Should Be $null
+	}
+
+	It "Should override configurations of the config file" {
+		$result = Get-PowerUpConfig "$here\etc\full_config.json" -Configuration @{ApplicationName = 'MyNewApp'; ConnectionTimeout = 3; Database = $null}
+		$result.ApplicationName | Should Be "MyNewApp"
+		$result.SqlInstance | Should Be "TestServer"
+		$result.Database | Should Be $null
+		$result.DeploymentMethod | Should Be "SingleTransaction"
+		$result.ConnectionTimeout | Should Be 3
 		$result.Encrypt | Should Be $null
 		$result.Credential | Should Be $null
 		$result.Username | Should Be "TestUser"
