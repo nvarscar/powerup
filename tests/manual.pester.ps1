32d64dfd096d1eb998105848490a59c570eb4dc1
Kirill Kravtsov <nvarscar@gmail.com>
2018-08-08 16:24:21 -0500

    Removing commented lines

--- tests/manual.pester.ps1
+++ tests/manual.pester.ps1
@@ -1,33 +1,4 @@
-﻿<#
-	.SYNOPSIS
-		A brief description of the Test-Module.ps1 file.
-	
-	.DESCRIPTION
-		The Test-Module.ps1 script lets you test the functions and other features of
-		your module in your PowerShell Studio module project. It's part of your project,
-		but it is not included in your module.
-		
-		In this test script, import the module (be careful to import the correct version)
-		and write commands that test the module features. You can include Pester
-		tests, too.
-		
-		To run the script, click Run or Run in Console. Or, when working on any file
-		in the project, click Home\Run or Home\Run in Console, or in the Project pane,
-		right-click the project name, and then click Run Project.
-	
-	.PARAMETER Path
-		A description of the Path parameter.
-	
-	.NOTES
-		===========================================================================
-		Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.145
-		Created on:   	11/21/2017 1:11 PM
-		Created by:   	kkravtsov
-		Organization:
-		Filename:     	Test-Module.ps1
-		===========================================================================
-#>
-param
+﻿Param
 (
 	[string[]]$Path = '.',
 	[string[]]$Tag
@@ -52,10 +23,3 @@ if ($Tag) {
 	$params += @{ Tag = $Tag}
 }
 Invoke-Pester @params
-
-#Sample Pester Test
-#Describe "Test dbops" {
-#	It "tests Write-HellowWorld" {
-#		Write-HelloWorld | Should BeExactly "Hello World"
-#	}	
-#}
